72d348350ffb571430b4c27bbeaee3c784c07b84
Rajan Mishra <rajanmishra2010@hotmail.com>
2016-09-21 16:29:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,31 +12,59 @@ var http = require('http');
 			
 			if(req.method=='POST'){
 				req.on('data', function (data) {
-	            body += data;
-		            if (body.length > 1e6)
-		                req.connection.destroy();
-	        	});
+	            			body += data;
+		            	if (body.length > 1e6)
+		                	req.connection.destroy();
+	        		});
 
 				req.on('end', function () {
 	    				var post = qs.parse(body);
-	    				var d = new Date();
-	    				console.log(d);
-						var n = d.getTime()+30000;
-						console.log("<br>"+n);
+	    				res.writeHead(200, {"Content-Type": "application/json"});
+	    				res.end( JSON.stringify({"success":true,"data":post}));
+	  			});
+			}
+			
+			
+			
+			if(req.method=='GET'){
+				req.on('data', function (data) {
+	            			body += data;
+		            	if (body.length > 1e6)
+		                	req.connection.destroy();
+	        		});
 
-						var data = {auctionId: "SALESAUCTION_ed927e0f662a9f4404f11486df62bbmb",
-						assetId:"fbf62851-82d1-483e-9c1b-963eb8938c86",
-						auctionGroup:"db03cd6b-a2a8-4c8e-9bdb-b442c7e2272a",
-						duration:60,
-						geofence:"",
-						geostatus:"INSIDE_OUTSIDE",
-						participants:0,
-						preRollHasImage:true,
-						preRollSeconds:40,preRollVideoUrl:"https://s3-us-west-2.amazonaws.com/assets.dropit.xyz/video/DropitDemoLevis.mp4",startprice:579,
-						starttime:n+30000,state:0,tickerTape:"<< Push in to play - hold down for 3 seconds - swipe up to buy <<",
-						time:n+30000,title:"hello <br> Auction.."}
+				req.on('end', function () {
+	    				var post = qs.parse(body);
+	    				res.writeHead(200, {"Content-Type": "application/json"});
+	    				res.end( JSON.stringify({"success":true,"data":post}));
+	  			});
+			}
+			
+			
+			if(req.method=='DELETE'){
+				req.on('data', function (data) {
+	            			body += data;
+		            	if (body.length > 1e6)
+		                	req.connection.destroy();
+	        		});
 
-	    				drivers.timeQueue.scheduleJobAt("auction", "initAuction", data, n, false);
+				req.on('end', function () {
+	    				var post = qs.parse(body);
+	    				res.writeHead(200, {"Content-Type": "application/json"});
+	    				res.end( JSON.stringify({"success":true,"data":post}));
+	  			});
+			}
+			
+			
+			if(req.method=='PUT'){
+				req.on('data', function (data) {
+	            			body += data;
+		            	if (body.length > 1e6)
+		                	req.connection.destroy();
+	        		});
+
+				req.on('end', function () {
+	    				var post = qs.parse(body);
 	    				res.writeHead(200, {"Content-Type": "application/json"});
 	    				res.end( JSON.stringify({"success":true,"data":post}));
 	  			});
